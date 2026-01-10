cc40a411e5c3ef1d9db0f18ad96cd4f416f7b29a
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2026-01-10 19:45:02 +0100

    Simplify script and treat more corner cases

--- main.jl
+++ main.jl
@@ -1,44 +1,16 @@
 using BestieTemplate
+using YAML
 
 const package_owner = "JuliaBesties"
 const user_name = "Bestie Template"
 const user_email = "me@bestie.tmp"
 const authors = "$user_name <$user_email>"
 
-function _git_setup()
-  run(`git init -q`)
-  run(`git add .`)
-  run(`git config user.name "$user_name"`)
-  run(`git config user.email "$user_email"`)
-  run(`git commit -q -m "First commit"`)
-end
-
-function _precommit()
-  try
-    read(`pre-commit run -a`) # run and ignore output
-  catch
-    nothing
-  end
-end
-
-function _full_precommit()
-  run(`git add .`)
-  _precommit()
-  run(`git add .`)
-  try
-    read(`git diff-index --exit-code HEAD`)
-    return # No commit necessary, exit now (consequence of pre-commit passing for default values)
-  catch
-    nothing
-  end
-  run(`git commit -q -m "git add . and pre-commit run -a"`)
-end
-
 # Main script
 for (i, strategy) in enumerate((:tiny, :light, :moderate, :robust))
   @info "Strategy $strategy"
 
-  package_name = "StrategyLevel$(i - 1)-" * titlecase(string(strategy)) * ".jl"
+  package_name = "StrategyLevel$(i - 1)" * titlecase(string(strategy)) * ".jl"
   package_path = joinpath(@__DIR__, package_name)
 
   if isdir(package_path)
@@ -52,17 +24,45 @@ for (i, strategy) in enumerate((:tiny, :light, :moderate, :robust))
     package_owner,
     authors,
     strategy,
+    template_source = :local,
   )
 
   @assert isdir(package_path)
 
   cd(package_path) do
-    @info "Creating first commit in repo"
-    _git_setup()
+    answers = YAML.load_file(".copier-answers.yml")
+    bestie_version = answers["_commit"]
+
+    @info "git add $package_name"
+    run(`git add .`)
+
+    if !success(`git diff-index --exit-code HEAD .`)
+      @info "Committing"
+      run(`git commit -m "Add $package_name from BestieTemplate@$bestie_version"`)
+    else
+      @warn "Nothing to commit"
+    end
 
     if isfile(".pre-commit-config.yml")
-      @info "Running pre-commit"
-      _full_precommit()
+      max_precommit_runs = 3
+      pre_commit_runs = 0
+      while pre_commit_runs < max_precommit_runs
+        @info "Running $package_name's pre-commit"
+        pre_commit_runs += 1
+        pre_commit_success = success(`pre-commit run -a`)
+
+        if pre_commit_success
+          @info "Successfully ran pre-commit"
+          break
+        else
+          @warn "pre-commit failed execution #$pre_commit_runs"
+        end
+      end
+      run(`git add .`)
+      if !success(`git diff-index --exit-code HEAD. `)
+        @info "Adding modifications"
+        run(`git commit -m "Add $package_name after running its pre-commit"`)
+      end
     end
   end
 end
